15a7b647b6fadb82eec74015c9451ce377868356
VadimPushtaev <pushtaev.vm@gmail.com>
2018-02-09 02:36:12 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dice_parser',
+    version='0.01',
+    packages=['dice_parser'],
+    url='https://github.com/VadimPushtaev/dice_parser',
+    license='MIT',
+    author='Vadim Pushtaev',
+    author_email='pushtaev.vm@gmail.com',
+    description='Arithmetic expressions with dice roll support'
+)
