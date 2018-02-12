ee6600f1fb47203dd219bc0bd19a031ee98e99cf
VadimPushtaev <pushtaev.vm@gmail.com>
2018-02-13 01:59:30 +0300

    highest and lowest support

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dice_parser',
-    version='0.3',
+    version='0.4',
     packages=['dice_parser'],
     install_requires=['lark-parser'],
     url='https://github.com/VadimPushtaev/dice_parser',
